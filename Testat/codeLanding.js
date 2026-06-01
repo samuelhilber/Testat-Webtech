31653f1b21772d9fcd0c713447b17999f29a3dd7
samuelhilber <sam.hilber@gmail.com>
2026-06-01 16:57:21 +0200

    landing auch automatisiert

--- Testat/codeLanding.js
+++ Testat/codeLanding.js
@@ -4,12 +4,34 @@ window.addEventListener('DOMContentLoaded', () => {
             return response.json();
         })
         .then((json) => {
-            for (let i = 0; i < json.length; i++) {
-                let idNummer = i + 1;
-                let idNameTitel = "Titel" + idNummer.toString();
-                const a = document.getElementById(idNameTitel);
-                a.innerText = json[i].Titel;
-                console.log(idNameTitel)
+            const cotainer = document.getElementById("landingContainer")
+
+            for (let i = 0; i <= 2; i++) {
+
+                const artikel = json[i]
+
+                const landingBox = document.createElement("div");
+                landingBox.classList.add("box-landing");
+
+                const bild = document.createElement("img");
+                bild.classList.add("newsartikelbild-landing");
+                bild.src = artikel.Bild;
+
+                const titel = document.createElement("div");
+                titel.classList.add("ueaberschriftNews")
+                titel.innerText = artikel.Titel;       
+
+                const button = document.createElement("a")
+                button.classList.add("btn");
+                button.href = "news.html"; 
+                button.innerText="Zu den News";
+                
+                landingBox.appendChild(bild);
+                landingBox.appendChild(titel);
+                landingBox.appendChild(button);
+                
+                cotainer.appendChild(landingBox);
+
             }
         });
 });
